c942f21f0065b122d7b56308ac4c886e152b5601
Moogeon <junmason@naver.com>
2022-12-13 23:31:36 +0900

    Write post 구현 로컬 땡길 커밋

--- src/main/java/dev/test/take_a_walk_duo/controllers/BbsController.java
+++ src/main/java/dev/test/take_a_walk_duo/controllers/BbsController.java
@@ -10,10 +10,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,7 +31,8 @@ public class BbsController {
     @RequestMapping(value = "write",
             method = RequestMethod.GET,
             produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView getWrite(@SessionAttribute(value = "user", required = false) UserEntity user, @RequestParam(value = "bid", required = false) String bid) {
+    public ModelAndView getWrite(@SessionAttribute(value = "user", required = false) UserEntity user,
+                                 @RequestParam(value = "bid", required = false) String bid) {
         ModelAndView modelAndView;
         if (user == null) {
             modelAndView = new ModelAndView("redirect:/member/login");
@@ -51,23 +49,37 @@ public class BbsController {
 
     //Mr.g
     //글쓰기 post(글작업하기)
-    @RequestMapping(value = "wirte", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String postWrite(@SessionAttribute(value = "user", required = false) UserEntity user, @RequestParam(value = "bid", required = false) String bid, ArticleEntity article, @RequestParam(value = "images", required = false) MultipartFile[] images) throws IOException {
+    @RequestMapping(value = "write", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String postWrite(@SessionAttribute(value = "user", required = false) UserEntity user,
+                            ArticleEntity article,
+                            @RequestParam(value = "images", required = false) MultipartFile[] images) throws IOException {
         Enum<?> result;
         JSONObject responseObject = new JSONObject();
         if (user == null) {
             result = WriteResult.NOT_ALLOWED;
-        } else if (bid == null) {
+        } else if (article.getBoardId() == null) {
             result = WriteResult.NO_SUCH_BOARD;
+        } else {
+            article.setUserEmail(user.getEmail());
+            result = this.bbsService.RegisterArticle(article, images);
         }
-        article.setUserEmail(user.getEmail());
-        article.setBoardId(bid);
-        result = this.bbsService.RegisterArticle(article, images);
         if (result == CommonResult.SUCCESS) {
             responseObject.put("aid", article.getIndex());
         }
         responseObject.put("result", result.name().toLowerCase());
+        System.out.println("controller check" + article.getBoardId());
         return responseObject.toString();
+    }
+
+    //Mr.g
+    @RequestMapping(value = "read",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getRead(@SessionAttribute(value = "user", required = false) UserEntity user,
+                                @RequestParam(value = "aid", required = false) int aid) {
+        ModelAndView modelAndView = new ModelAndView("bbs/read");
+        return modelAndView;
     }
 
 
@@ -77,15 +89,6 @@ public class BbsController {
             produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getList() {
         ModelAndView modelAndView = new ModelAndView("bbs/list");
-        return modelAndView;
-    }
-
-    //Mr.g
-    @RequestMapping(value = "read",
-            method = RequestMethod.GET,
-            produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView getRead() {
-        ModelAndView modelAndView = new ModelAndView("bbs/read");
         return modelAndView;
     }
 
--- src/main/resources/static/bbs/resources/scripts/write.js
+++ src/main/resources/static/bbs/resources/scripts/write.js
@@ -29,34 +29,8 @@ form.addEventListener('input', () => {
     }
 });
 
-form.querySelector('[rel="contentImageContainer"]').addEventListener('click', e => {
-    e.preventDefault();
-    form['images'].click();
-});
-
-
-form.addEventListener('input', () => {
-    const imageContainerElement = form.querySelector('[rel="contentImageContainer"]');
-    imageContainerElement.querySelectorAll('img.image').forEach(x => x.remove());
-    if (form['images'].files.length > 0) {
-        form.querySelector('[rel="imageSelectButton"]').setAttribute('hidden','hidden');
-        form.querySelector('[rel="noImage"]').setAttribute('hidden','hidden');
-    } else {
-        form.querySelector('[rel="noImage"]').classList.remove('hidden')
-    }
-    for (let file of form['images'].files) {
-        const imageSrc = URL.createObjectURL(file);
-        const imgElement = document.createElement('img');
-        imgElement.classList.add('image');
-        imgElement.setAttribute('src', imageSrc);
-        imageContainerElement.append(imgElement);
-    }
-});
-
-
 form.onsubmit = e => {
     e.preventDefault();
-    Warning.hide();
 
     if (form['title'].value === '') {
         alert('제목을 입력해주세요!!!');
@@ -73,7 +47,10 @@ form.onsubmit = e => {
     // Cover.show('게시글을 작성하는 중입니다.')
     const xhr = new XMLHttpRequest();
     const formData = new FormData;
-    formData.append('bid', form['bid'].value);
+    for (let file of form['images'].files) {
+        formData.append('images', file);
+    }
+    formData.append('boardId', form['criterion'].value);
     formData.append('title', form['title'].value);
     formData.append('content', editor.getData());
     xhr.open('POST', './write');
@@ -85,7 +62,7 @@ form.onsubmit = e => {
                 const responseObject = JSON.parse(xhr.responseText);
                 switch (responseObject['result']) {
                     case 'not_allowed':
-                        Warning.show('게시글을 작성할 수 있는 권한이 없거나 로그아웃 되었습니다. 확인 후 다시 시도해 주세요.');
+                        console.log('게시글을 작성할 수 있는 권한이 없거나 로그아웃 되었습니다. 확인 후 다시 시도해 주세요.');
                         break;
                     case 'success':
                         // window.location.href = '/read?aid=' + responseObject['aid'];
@@ -93,10 +70,10 @@ form.onsubmit = e => {
                         window.location.href = `read?aid=${aid}`;
                         break;
                     default:
-                        Warning.show('알수 없는 이유로 게시글을 작성하지 못하였습니다. 잠시 후 다시 시도해 주세요.')
+                        console.log('알수 없는 이유로 게시글을 작성하지 못하였습니다. 잠시 후 다시 시도해 주세요.')
                 }
             } else {
-                Warning.show('서버와 통신하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
+                console.log('서버와 통신하지 못하였습니다. 잠시 후 다시 시도해 주세요.');
             }
         }
     };
