417197d3f1df51dbba366768621436011b854828
Moogeon <junmason@naver.com>
2023-01-12 13:09:47 +0900

    main back 구현ing

--- src/main/java/dev/twd/take_a_walk_duo/controllers/HomeController.java
+++ src/main/java/dev/twd/take_a_walk_duo/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package dev.twd.take_a_walk_duo.controllers;
 
+import dev.twd.take_a_walk_duo.services.HomeService;
+import dev.twd.take_a_walk_duo.vos.bbs.ArticleReadVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +13,19 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/")
 public class HomeController extends GeneralController {
 
+    private final HomeService homeService;
+
+    @Autowired
+    public HomeController(HomeService homeService) {
+        this.homeService = homeService;
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET,
             produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView getHome(){
+    public ModelAndView getHome() {
         ModelAndView modelAndView = new ModelAndView("home/index");
+        ArticleReadVo[] recentArticle = this.homeService.getRecentArticles("free");
+        modelAndView.addObject("articles", recentArticle);
         return modelAndView;
     }
 }
