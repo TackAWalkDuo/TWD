6885405a11589523e6ecde4241ef4efc4fd80417
been <wjscdksqls@gmail.com>
2023-01-25 11:11:12 +0900

    23.01.25 properties 제외

--- src/main/java/dev/twd/take_a_walk_duo/enums/member/ModifyUserResult.java
+++
@@ -1,9 +0,0 @@
-package dev.twd.take_a_walk_duo.enums.member;
-
-import dev.twd.take_a_walk_duo.interfaces.IResult;
-
-public enum ModifyUserResult implements IResult {
-    NO_SUCH_USER,   // 유저가 없음
-    NOT_ALLOWED,    // 권한이 없음
-    NOT_SIGNED      // 로그인하지 않음
-}
--- src/main/java/dev/twd/take_a_walk_duo/enums/member/VerifyEmailAuthResult.java
+++
@@ -1,7 +0,0 @@
-package dev.twd.take_a_walk_duo.enums.member;
-
-import dev.twd.take_a_walk_duo.interfaces.IResult;
-
-public enum VerifyEmailAuthResult implements IResult {
-    EXPIRED
-}
